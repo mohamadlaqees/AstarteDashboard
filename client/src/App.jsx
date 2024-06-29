b37ca4eaa70f2e4328d68d08f4d0ff908fe9b629
mohamadlaqees <mohamadlk24@gmail.com>
2024-06-29 23:10:33 +0300

    adjust routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,64 @@
-import { CssBaseline, ThemeProvider,  } from "@mui/material";
-import {createTheme} from "@mui/material/styles"
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { themeSettings } from "./theme";
-import Dashboard from "./scenes/Dashboard/Dashboard";
-import Layout from "./scenes/Layout/Layout";
-import Products from "./scenes/Products/Products"
-import Customers from "./scenes/Customers/Customers"
-import Transactions from "./scenes/Transactions/Transactions";
-import Geography from "./scenes/Geography/Geography";
-import Overview from "./scenes/Overview/Overview";
-import Daily from "./scenes/Daily/Daily";
-import Monthly from "./scenes/Monthly/Monthly";
-import Breakdown from "./scenes/Breakdown/Breakdown";
-import Admins from "./scenes/Admins/Admins";
-import Performance from "./scenes/Performance/Performance";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import useMode from "./hooks/useMode";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Users from "./pages/Users/Users";
+import Artifacts from "./pages/Artifacts/Artifacts";
+import Layout from "./Layout/Layout";
 
-function App() {
-const mode = useSelector((state) => state.global.mode)
-const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
+const App = () => {
+  const { theme } = useMode();
+
+  const routes = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+
+        {
+          path: "artifacts",
+          element: <Artifacts />,
+        },
+
+        {
+          path: "tours",
+          element: <Users />,
+        },
+
+        {
+          path: "payments",
+          element: <Users />,
+        },
+
+        {
+          path: "funds",
+          element: <Users />,
+        },
+
+        {
+          path: "reports",
+          element: <Users />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="app">
-      <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route element={<Layout />} >
-          <Route path ="/" element={<Navigate to="dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/geography" element={<Geography />} />
-          <Route path="/overview" element={<Overview />} />
-          <Route path="/daily" element={<Daily />} />
-          <Route path="/monthly" element={<Monthly />} />
-          <Route path="/breakdown" element={<Breakdown />} />
-          <Route path="/admin" element={<Admins />} />
-          <Route path="/performance" element={<Performance />} />
-
-
-          </Route>
-        </Routes>
-      </ThemeProvider>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={routes} />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
