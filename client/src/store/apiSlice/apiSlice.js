5040c06bea7c05fb44ab98734474a78c61a4462e
mohamadlaqees <mohamadlk24@gmail.com>
2024-08-12 18:08:32 +0300

    initial projects query

--- client/src/store/apiSlice/apiSlice.js
+++ client/src/store/apiSlice/apiSlice.js
@@ -5,26 +5,42 @@ export const apiSlice = createApi({
 
   baseQuery: fetchBaseQuery({
     baseUrl: " https://hp-backend-741l.onrender.com/api/",
-    mode:'no-cors'
+    mode: "no-cors",
   }),
 
   endpoints: (build) => ({
     getAllExperiences: build.query({
       query: () => "experiences",
-      
     }),
-
+    getExperience: build.query({
+      query: (id) => `experiences/${id}`,
+    }),
     addExperience: build.query({}),
-
     updateExperience: build.query({}),
-
     deleteExperience: build.query({}),
+
+    // projects
+    getAllProjects: build.query({
+      query: () => "projects",
+    }),
+    getProject: build.query({
+      query: (id) => `projects/${id}`,
+    }),
+    addProject: build.query({}),
+    updateProject: build.query({}),
+    deleteProject: build.query({}),
   }),
 });
 
 export const {
   useGetAllExperiencesQuery,
+  useGetExperienceQuery,
   useAddExperienceQuery,
   useUpdateExperienceQuery,
   useDeleteExperienceQuery,
+  useGetAllProjectsQuery,
+  useGetProjectQuery,
+  useAddProjectQuery,
+  useUpdateProjectQuery,
+  useDeleteProjectQuery,
 } = apiSlice;
