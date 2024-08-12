8a51caa01ba3596b1af84dd5c57cf30573adaf01
mohamadlaqees <mohamadlk24@gmail.com>
2024-08-12 18:50:36 +0300

    initial Project page

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import React from "react";
+import { Box, useTheme, IconButton } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Header from "../../components/Header";
+import { dataProject } from "./dataProject";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const theme = useTheme();
 
-export default Projects
+  const handleEdit = (id) => {
+    console.log(`Edit user with ID: ${id}`);
+  };
+
+  const handleDelete = (id) => {
+    console.log(`Delete user with ID: ${id}`);
+  };
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      flex: 1,
+    },
+    {
+      field: "location",
+      headerName: "Location",
+      flex: 1,
+    },
+    {
+      field: "startingPoint",
+      headerName: "StartingPoint",
+      flex: 1,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Box>
+            <IconButton
+              onClick={() => handleEdit(params.row._id)}
+              color="secondary"
+            >
+              <EditIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => handleDelete(params.row._id)}
+              color="secondary"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Projects" subtitle="List of Projects" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          // loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={dataProject || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Projects;
