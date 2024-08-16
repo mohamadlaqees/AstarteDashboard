12671b0883a235a92cfa33b54579be919d1f8315
mohamadlaqees <mohamadlk24@gmail.com>
2024-08-16 15:57:41 +0300

    update dataProject

--- client/src/pages/Projects/dataProject.js
+++ client/src/pages/Projects/dataProject.js
@@ -2,8 +2,11 @@ export const dataProject = [
   {
     _id: "63701cc1f03239c72c00017f",
     name: "Konstantine",
+    isEducational: false,
     description: "Konstantine Konstantine Konstantine",
     location: "2 days",
     startingPoint: "test",
+    createdAt: "2024-05-20T10:03:06.293Z",
+    updatedAt: "2024-05-20T10:03:06.293Z",
   },
 ];
