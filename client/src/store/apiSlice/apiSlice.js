f32cfdea5520c87f693dffa123c651ee8bca1fe8
mohamadlaqees <mohamadlk24@gmail.com>
2024-08-18 22:48:32 +0300

    finish the endPoints

--- client/src/store/apiSlice/apiSlice.js
+++ client/src/store/apiSlice/apiSlice.js
@@ -15,9 +15,26 @@ export const apiSlice = createApi({
     getExperience: build.query({
       query: (id) => `experiences/${id}`,
     }),
-    addExperience: build.query({}),
-    updateExperience: build.query({}),
-    deleteExperience: build.query({}),
+    addExperience: build.mutation({
+      query: (newExperience) => ({
+        url: "experiences",
+        method: "POST",
+        body: newExperience,
+      }),
+    }),
+    updateExperience: build.mutation({
+      query: ({ updatedExperience, id }) => ({
+        url: `experiences/${id}`,
+        method: "PATCH",
+        body: updatedExperience,
+      }),
+    }),
+    deleteExperience: build.mutation({
+      query: (id) => ({
+        url: `experiences/${id}`,
+        method: "DELETE",
+      }),
+    }),
 
     // projects
     getAllProjects: build.query({
@@ -26,9 +43,26 @@ export const apiSlice = createApi({
     getProject: build.query({
       query: (id) => `projects/${id}`,
     }),
-    addProject: build.query({}),
-    updateProject: build.query({}),
-    deleteProject: build.query({}),
+    addProject: build.mutation({
+      query: (newProject) => ({
+        url: "projects",
+        method: "POST",
+        body: newProject,
+      }),
+    }),
+    updateProject: build.mutation({
+      query: ({ updatedProject, id }) => ({
+        url: `projects/${id}`,
+        method: "PATCH",
+        body: updatedProject,
+      }),
+    }),
+    deleteProject: build.mutation({
+      query: (id) => ({
+        url: `projects/${id}`,
+        method: "DELETE",
+      }),
+    }),
 
     // impact funds
     getAllImpactFunds: build.query({
@@ -37,26 +71,43 @@ export const apiSlice = createApi({
     getFund: build.query({
       query: (id) => `impact-funds/${id}`,
     }),
-    addFund: build.query({}),
-    updateFund: build.query({}),
-    deleteFund: build.query({}),
+    addFund: build.mutation({
+      query: (newFund) => ({
+        url: "impact-funds",
+        method: "POST",
+        body: newFund,
+      }),
+    }),
+    updateFund: build.mutation({
+      query: ({ updatedFund, id }) => ({
+        url: `impact-funds/${id}`,
+        method: "PATCH",
+        body: updatedFund,
+      }),
+    }),
+    deleteFund: build.mutation({
+      query: (id) => ({
+        url: `impact-funds/${id}`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
 export const {
   useGetAllExperiencesQuery,
   useGetExperienceQuery,
-  useAddExperienceQuery,
-  useUpdateExperienceQuery,
-  useDeleteExperienceQuery,
+  useAddExperienceMutation,
+  useUpdateExperienceMutation,
+  useDeleteExperienceMutation,
   useGetAllProjectsQuery,
   useGetProjectQuery,
-  useAddProjectQuery,
-  useUpdateProjectQuery,
-  useDeleteProjectQuery,
+  useAddProjectMutation,
+  useUpdateProjectMutation,
+  useDeleteProjectMutation,
   useGetAllImpactFundsQuery,
   useGetFundQuery,
-  useAddFundQuery,
-  useUpdateFundQuery,
-  useDeleteFundQuery,
+  useAddFundMutation,
+  useUpdateFundMutation,
+  useDeleteFundMutation,
 } = apiSlice;
